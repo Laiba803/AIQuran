aa21c5f9754a11be27fad6b96e1146fac550711a
laiba803 <laibanoorali803@gmail.com>
2022-09-07 03:16:07 +0500

    layout completed

---
+++ app/src/main/java/com/example/quranapp2/SurahNames.java
@@ -0,0 +1,14 @@
+package com.example.quranapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SurahNames extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_surah_names);
+    }
+}
